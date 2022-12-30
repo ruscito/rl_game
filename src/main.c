68258127783ebbc2d466e6d17e1e87c2bf151eb8
ruscito <ruscito@gmail.com>
2022-12-30 14:57:22 -0600

    First commit

---
+++ src/main.c
@@ -0,0 +1,46 @@
+#include "raylib.h"
+
+#define SCREEN_WIDTH (800)
+#define SCREEN_HEIGHT (450)
+
+#define WINDOW_TITLE "The Game Zero"
+
+void start_engine() 
+{
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, WINDOW_TITLE);
+    SetTargetFPS(60);
+}
+
+void shutdown_engine() 
+{
+    CloseWindow();
+}
+
+
+int main(void)
+{
+    start_engine();
+
+    Texture2D texture = LoadTexture(ASSETS_PATH"test.png"); // Check README.md for how this works
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+
+        ClearBackground(RAYWHITE);
+
+        const int texture_x = SCREEN_WIDTH / 2 - texture.width / 2;
+        const int texture_y = SCREEN_HEIGHT / 2 - texture.height / 2;
+        DrawTexture(texture, texture_x, texture_y, WHITE);
+
+        const char* text = "OMG! IT WORKS!";
+        const Vector2 text_size = MeasureTextEx(GetFontDefault(), text, 20, 1);
+        DrawText(text, SCREEN_WIDTH / 2 - text_size.x / 2, texture_y + texture.height + text_size.y + 10, 20, BLACK);
+
+        EndDrawing();
+    }
+
+    shutdown_engine();
+
+    return 0;
+}
